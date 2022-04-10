6a9d1027e2b1e18554b55e26424a4c71b69db109
NMINHDAI <ngominhdai92tn@gmail.com>
2022-04-10 11:22:05 +0700

    A

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose.connect(
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-app.use(express.json());
+app.use(express.json({ limit: '30mb', extended: true }));
 app.use(helmet());
 app.use(morgan("common"));
 
@@ -47,7 +47,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
     console.error(error);
   }
 });
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({limit: '30mb', extended:true}));
 app.use(cors());
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
