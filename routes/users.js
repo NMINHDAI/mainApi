9d4ac03c5c6fc9b19c321aca323b65e6af6152ea
NMINHDAI <ngominhdai92tn@gmail.com>
2022-04-12 17:43:31 +0700

    A

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const UserModal = require("../models/User");
-
+const Conversation = require("../models/Conversation");
 const secret = 'test';
 
 router.post("/signin",async (req, res) => {
@@ -14,21 +14,21 @@ router.post("/signin",async (req, res) => {
   try {
     const oldUser = await UserModal.findOne({ email });
 
-    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+    if (!oldUser) return res.status(404).json({ statusCode: 404, message: "User doesn't exist" });
     else {
       const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-      if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+      if (!isPasswordCorrect) return res.status(400).json({ statusCode: 400, message: "Invalid credentials" });
       else {
         const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
 
-        res.status(200).json({ result: oldUser, token });
+        res.status(200).json({ statusCode: 200, message: "success", result: oldUser, token });
       }
       
     }
     
   } catch (err) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ statusCode: 500, message: "Something went wrong" });
   }
 });
 
@@ -38,19 +38,30 @@ router.post("/signup", async (req, res) => {
   try {
     const oldUser = await UserModal.findOne({ email });
 
-    if (oldUser) return res.status(400).json({ message: "User already exists" });
+    if (oldUser) return res.status(400).json({ statusCode: 400, message: "User already exists" });
     else {
       const hashedPassword = await bcrypt.hash(password, 12);
 
       const result = await UserModal.create({ email, password: hashedPassword, name: name });
   
       const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
-  
-      res.status(201).json({ result, token });
+      
+      //create conversation with admin
+      const newConversation = new Conversation({
+        members: [result._id, process.env.adminId],
+      });
+    
+      try {
+        await newConversation.save();
+      } catch (err) {
+        res.status(500).json(err);
+      }
+
+      res.status(201).json({ statusCode: 201, message: "success", result, token });
     }
    
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ statusCode:500, message: "Something went wrong" });
     
     console.log(error);
   }
